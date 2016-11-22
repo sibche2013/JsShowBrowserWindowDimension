7d5388232a8ba3cb7ec6dae8693919a91885cf39
Amin Arjmand <aminarj2000@gmail.com>
2016-11-22 09:28:59 +0330

    Add files via upload

---
+++ showBrowserWindowDimension.js
@@ -0,0 +1,3 @@
+﻿    function resize() {
+        document.getElementById('btn').innerHTML = "Width Of Browser Is: " + innerWidth + "px" + "<br>Height Of Browser Is: " + innerHeight + "px<br><br>"+innerWidth+"px * "+innerHeight+"px";
+    }
